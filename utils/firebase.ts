2418bfe9db0d3e3698b0e52d13583004a91590e2
Azim Ahmed <azim.ahmed7@gmail.com>
2024-03-26 14:01:48 -0400

    3.1 React Native + Firebase

---
+++ utils/firebase.ts
@@ -0,0 +1,29 @@
+import { FirebaseOptions, initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getFunctions } from 'firebase/functions';
+import { getAuth } from 'firebase/auth';
+
+const {
+	EXPO_PUBLIC_API_KEY,
+  EXPO_PUBLIC_AUTH_DOMAIN,
+  EXPO_PUBLIC_PROJECT_ID,
+  EXPO_PUBLIC_STORAGE_BUCKET,
+  EXPO_PUBLIC_MESSAGING_SENDER_ID,
+  EXPO_PUBLIC_APP_ID,
+  EXPO_PUBLIC_MEASUREMENT_ID,
+} = process.env;
+
+const firebaseOptions: FirebaseOptions = {
+	apiKey: EXPO_PUBLIC_API_KEY,
+  authDomain: EXPO_PUBLIC_AUTH_DOMAIN,
+  projectId: EXPO_PUBLIC_PROJECT_ID,
+  storageBucket: EXPO_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: EXPO_PUBLIC_MESSAGING_SENDER_ID,
+  appId: EXPO_PUBLIC_APP_ID,
+  measurementId: EXPO_PUBLIC_MEASUREMENT_ID,
+}
+
+export const app = initializeApp(firebaseOptions);
+export const db = getFirestore(app);
+export const functions = getFunctions(app);
+export const auth = getAuth(app);
