7bcb25c738a7c63cbec666eefa9be3a713286d12
Azim Ahmed <azim.ahmed7@gmail.com>
2024-04-10 14:13:47 -0400

    5.4 Setup Accordion with categories

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,31 @@
 import { Container } from "@/components/Container";
 import { useDataProvider } from "@/components/DataProvider";
-import { Text } from "tamagui";
+import { Accordion, Paragraph, Text } from "tamagui";
 
 export default function MenuScreen() {
   const { categories } = useDataProvider();
+
+  if (!categories) return null;
+
   return (
     <Container>
-      {categories?.map((category) => (
-        <Text>{category?.title}</Text>
-      ))}
+      <Accordion
+        overflow="hidden"
+        type="multiple"
+        defaultValue={categories.map(({ id }) => id)}
+        disabled
+      >
+        {categories.map((category) => (
+          <Accordion.Item value={category.id} key={category.id}>
+            <Accordion.Trigger>
+              <Paragraph>{category.title}</Paragraph>
+            </Accordion.Trigger>
+            <Accordion.Content p={0}>
+              <Text>This is fake content</Text>
+            </Accordion.Content>
+          </Accordion.Item>
+        ))}
+      </Accordion>
     </Container>
   );
 }
