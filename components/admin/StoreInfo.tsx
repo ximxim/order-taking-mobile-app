06fb2bbfe9d202ac241efdabcd9b8597c60febae
Azim Ahmed <azim.ahmed7@gmail.com>
2024-04-10 11:38:13 -0400

    4.11 Wire up restraunt info form

--- components/admin/StoreInfo.tsx
+++ components/admin/StoreInfo.tsx
@@ -5,17 +5,51 @@ import {
   SimpleForm,
   TextInput,
   TimeInput,
+  useNotify,
 } from "react-admin";
+import { db } from "@/utils/firebase";
+import { doc, setDoc, getDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
 
-export const StoreInfo = () => (
-  <Card>
-    <SimpleForm>
-      <TextInput source="name" fullWidth />
-      <TextInput source="address" fullWidth />
-      <TextInput source="phone" fullWidth />
-      <TimeInput source="openingTime" fullWidth />
-      <TimeInput source="closingTime" fullWidth />
-      <CheckboxGroupInput source="paymentMethods" choices={PAYMENT_METHODS} />
-    </SimpleForm>
-  </Card>
-);
+export const StoreInfo = () => {
+  const notify = useNotify();
+  const [defaultValues, setDefaultValues] = useState<any>();
+  const restaurantInfoRef = doc(db, "restaurant", "info");
+
+  const handleSubmit = async (data: any) => {
+    await setDoc(restaurantInfoRef, data);
+    notify(`Info updated`, { type: "success" });
+  };
+
+  const fetchData = async () => {
+    const snapshot = await getDoc(restaurantInfoRef);
+    setDefaultValues(snapshot.data() || {});
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (!defaultValues) return null;
+
+  return (
+    <Card>
+      <SimpleForm
+        defaultValues={{
+          ...defaultValues,
+          openingTime: defaultValues?.openingTime?.toDate?.(),
+          closingTime: defaultValues?.closingTime?.toDate?.(),
+        }}
+        onSubmit={handleSubmit}
+        sanitizeEmptyValues
+      >
+        <TextInput source="name" fullWidth />
+        <TextInput source="address" fullWidth />
+        <TextInput source="phone" fullWidth />
+        <TimeInput source="openingTime" fullWidth />
+        <TimeInput source="closingTime" fullWidth />
+        <CheckboxGroupInput source="paymentMethods" choices={PAYMENT_METHODS} />
+      </SimpleForm>
+    </Card>
+  );
+};
