a7b5a35522690a33a3cd2b872c94f1b093ce7823
Azim Ahmed <azim.ahmed7@gmail.com>
2024-04-11 19:48:17 -0400

    6.2 Item Variant form elements

--- app/item/[slug].tsx
+++ app/item/[slug].tsx
@@ -6,14 +6,64 @@ import {
   Stack,
   Text,
   TextArea,
+  ToggleGroup,
+  XStack,
   YStack,
 } from "tamagui";
 import { useLocalSearchParams, useNavigation } from "expo-router";
-import { useEffect } from "react";
+import { FC, useEffect } from "react";
 import { useDataProvider } from "@/components/DataProvider";
 import { Container } from "@/components/Container";
 import { useForm } from "react-hook-form";
-import { ILine } from "@/models";
+import { IChoice, ILine, IVariant } from "@/models";
+
+interface IVariantProps {
+  variant: IVariant;
+  index: number;
+}
+
+interface IChoiceProps {
+  choice: IChoice;
+  index: number;
+}
+
+const Variant: FC<IVariantProps> = ({ variant }) => {
+  return (
+    <YStack>
+      <Label>
+        <Text>{variant.type}</Text>
+        {variant.isRequired && (
+          <Text fontSize={12} ml="$1">
+            (Required)
+          </Text>
+        )}
+      </Label>
+      <ToggleGroup type="single">
+        <YStack alignItems="flex-start">
+          {variant.choices.map((choice, index) => (
+            <Choice choice={choice} index={index} />
+          ))}
+        </YStack>
+      </ToggleGroup>
+    </YStack>
+  );
+};
+
+const Choice: FC<IChoiceProps> = ({ choice }) => {
+  return (
+    <YStack px={4} py={2} mt="0px !important">
+      <XStack alignItems="center" gap="$4">
+        <ToggleGroup.Item value={choice.label}></ToggleGroup.Item>
+        <Label>
+          <Text>{choice.label}</Text>
+          <Text ml="$2" fontSize={12}>
+            +${choice.price}
+          </Text>
+        </Label>
+      </XStack>
+    </YStack>
+  );
+};
 
 export default function ItemScreen() {
   const { slug } = useLocalSearchParams();
@@ -53,6 +103,9 @@ export default function ItemScreen() {
             flex={1}
             source={{ uri: item.image.src, width: 280, height: 280 }}
           />
+          {item.variants.map((variant, index) => (
+            <Variant variant={variant} index={index} />
+          ))}
           <YStack>
             <Label>Special Instructions</Label>
             <TextArea
