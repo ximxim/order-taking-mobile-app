ddb29cc5bd100cf8f7e5864c73d9f7d05c95057b
Azim Ahmed <azim.ahmed7@gmail.com>
2024-04-12 14:47:31 -0400

    6.4 Item variant form wiring

--- app/item/[slug].tsx
+++ app/item/[slug].tsx
@@ -14,9 +14,14 @@ import { useLocalSearchParams, useNavigation } from "expo-router";
 import { FC, useEffect } from "react";
 import { useDataProvider } from "@/components/DataProvider";
 import { Container } from "@/components/Container";
-import { useForm } from "react-hook-form";
+import {
+  FormProvider,
+  useFieldArray,
+  useForm,
+  useFormContext,
+} from "react-hook-form";
 import { IChoice, ILine, IVariant } from "@/models";
-import { Circle } from "@tamagui/lucide-icons";
+import { Circle, CircleDot } from "@tamagui/lucide-icons";
 
 interface IVariantProps {
   variant: IVariant;
@@ -24,11 +29,31 @@ interface IVariantProps {
 }
 
 interface IChoiceProps {
+  variant: IVariant;
   choice: IChoice;
   index: number;
 }
 
 const Variant: FC<IVariantProps> = ({ variant }) => {
+  const { control } = useFormContext<ILine>();
+  const { append, fields, remove } = useFieldArray<ILine, "value">({
+    control,
+    name: "value",
+  });
+  console.log("fields", fields);
+  const handleValueChange = (index: string) => {
+    const removeAll = fields.reduce((acc, field: any, index) => {
+      if (field.variant !== variant.type) return acc;
+      return [...acc, index];
+    }, [] as number[]);
+    remove(removeAll);
+    const currIndex = parseInt(index.split(":")[1]);
+    append({
+      variant: variant.type,
+      price: variant.choices[currIndex].price,
+      value: variant.choices[currIndex].label,
+    });
+  };
   return (
     <YStack>
       <Label>
@@ -39,10 +64,14 @@ const Variant: FC<IVariantProps> = ({ variant }) => {
           </Text>
         )}
       </Label>
-      <ToggleGroup type="single">
+      <ToggleGroup
+        type="single"
+        disableDeactivation
+        onValueChange={handleValueChange}
+      >
         <YStack alignItems="flex-start">
           {variant.choices.map((choice, index) => (
-            <Choice choice={choice} index={index} />
+            <Choice variant={variant} choice={choice} index={index} />
           ))}
         </YStack>
       </ToggleGroup>
@@ -50,12 +79,20 @@ const Variant: FC<IVariantProps> = ({ variant }) => {
   );
 };
 
-const Choice: FC<IChoiceProps> = ({ choice, index }) => {
+const Choice: FC<IChoiceProps> = ({ choice, index, variant }) => {
+  const { watch } = useFormContext<ILine>();
+  const fields = watch("value");
+  const isSelected =
+    fields!.findIndex(
+      (f) =>
+        f.variant === variant.type &&
+        variant.choices.findIndex((c) => c.label === f.value) === index
+    ) > -1;
   return (
     <YStack px={4} py={2} mt="0px !important">
       <XStack alignItems="center" gap="$4">
-        <ToggleGroup.Item value={choice.label}>
-          <Circle />
+        <ToggleGroup.Item value={`${variant.type}:${index}`}>
+          {isSelected ? <CircleDot /> : <Circle />}
         </ToggleGroup.Item>
         <Label>
           <Text>{choice.label}</Text>
@@ -73,7 +110,7 @@ export default function ItemScreen() {
   const { setOptions } = useNavigation();
   const { getItemById } = useDataProvider();
   const item = getItemById(slug as string);
-  const { register, handleSubmit } = useForm<ILine>({
+  const methods = useForm<ILine>({
     defaultValues: {
       quantity: 1,
       value: [],
@@ -81,6 +118,7 @@ export default function ItemScreen() {
       label: item?.label,
     },
   });
+  const { register, handleSubmit } = methods;
 
   useEffect(() => {
     if (!item) return;
@@ -96,36 +134,38 @@ export default function ItemScreen() {
   if (!item) return null;
 
   return (
-    <Container>
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        <YStack gap={4} p={4}>
-          <Image
-            height="280px"
-            minWidth="280px"
-            width="100%"
-            flex={1}
-            source={{ uri: item.image.src, width: 280, height: 280 }}
-          />
-          {item.variants.map((variant, index) => (
-            <Variant variant={variant} index={index} />
-          ))}
-          <YStack>
-            <Label>Special Instructions</Label>
-            <TextArea
-              placeholder="pepper / salt / cutlury..."
-              {...register("instructions")}
+    <FormProvider {...methods}>
+      <Container>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <YStack gap={4} p={4}>
+            <Image
+              height="280px"
+              minWidth="280px"
+              width="100%"
+              flex={1}
+              source={{ uri: item.image.src, width: 280, height: 280 }}
             />
+            {item.variants.map((variant, index) => (
+              <Variant variant={variant} index={index} />
+            ))}
+            <YStack>
+              <Label>Special Instructions</Label>
+              <TextArea
+                placeholder="pepper / salt / cutlury..."
+                {...register("instructions")}
+              />
+            </YStack>
+            <YStack>
+              <Label>Quantity</Label>
+              <Input
+                keyboardType="number-pad"
+                defaultValue="1"
+                {...register("quantity", { valueAsNumber: true, min: 1 })}
+              />
+            </YStack>
           </YStack>
-          <YStack>
-            <Label>Quantity</Label>
-            <Input
-              keyboardType="number-pad"
-              defaultValue="1"
-              {...register("quantity", { valueAsNumber: true, min: 1 })}
-            />
-          </YStack>
-        </YStack>
-      </Form>
-    </Container>
+        </Form>
+      </Container>
+    </FormProvider>
   );
 }
