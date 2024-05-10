7ac002a290421094567dc474b4d78079d699d86d
Azim Ahmed <azim.ahmed7@gmail.com>
2024-05-10 14:04:36 -0400

    8.1 Skeleton accordion with comment form

--- app/checkout.tsx
+++ app/checkout.tsx
@@ -1,14 +1,84 @@
+import { BottomButton } from "@/components/BottomButton";
 import { Container } from "@/components/Container";
+import { useDataProvider } from "@/components/DataProvider";
+import { IOrder } from "@/models";
+import { calculateTotal } from "@/utils/calculations";
 import { useRouter } from "expo-router";
-import { Button, Stack, Text } from "tamagui";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import {
+  Accordion,
+  Button,
+  Form,
+  Label,
+  Paragraph,
+  Stack,
+  Text,
+  TextArea,
+} from "tamagui";
 
 export default function CheckoutScreen() {
+  const { lines } = useDataProvider();
+  const { handleSubmit, register, setValue } = useForm<IOrder>();
   const router = useRouter();
 
+  const onSubmit = (data: IOrder) => {
+    // TODO: navigate to thank you page once order is created
+    console.log("order", data);
+  };
+
+  useEffect(() => {
+    register("comments");
+  }, []);
+
   return (
-    <Container>
-      <Text>Checkout Screen</Text>
-      <Button onPress={() => router.replace("/thankYou")}>Next</Button>
-    </Container>
+    <>
+      <Container>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <Accordion
+            overflow="hidden"
+            type="multiple"
+            defaultValue={["CONTACT", "PAYMENT METHOD", "COMMENTS"]}
+            disabled
+          >
+            <Accordion.Item value="CONTACT">
+              <Accordion.Trigger>
+                <Paragraph>CONTACT</Paragraph>
+              </Accordion.Trigger>
+              <Accordion.Content>
+                <Paragraph>CONTACT</Paragraph>
+              </Accordion.Content>
+            </Accordion.Item>
+            <Accordion.Item value="PAYMENT METHOD">
+              <Accordion.Trigger>
+                <Paragraph>PAYMENT METHOD</Paragraph>
+              </Accordion.Trigger>
+              <Accordion.Content>
+                <Paragraph>PAYMENT METHOD</Paragraph>
+              </Accordion.Content>
+            </Accordion.Item>
+            <Accordion.Item value="COMMENTS">
+              <Accordion.Trigger>
+                <Paragraph>COMMENTS</Paragraph>
+              </Accordion.Trigger>
+              <Accordion.Content>
+                <Label>Comments</Label>
+                <TextArea
+                  placeholder="Comments..."
+                  onChangeText={(val) =>
+                    setValue("comments", val, { shouldValidate: true })
+                  }
+                />
+              </Accordion.Content>
+            </Accordion.Item>
+          </Accordion>
+        </Form>
+      </Container>
+      <BottomButton
+        onClick={handleSubmit(onSubmit)}
+        label="Go to checkout"
+        total={`$${calculateTotal(lines, 13).toFixed(2)}`}
+      />
+    </>
   );
 }
